6dce6f4379b2b0789754d39a54c4362bb1c60755
Saiyam Jain <95903449+saiyumm@users.noreply.github.com>
2023-03-16 18:19:10 +0530

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { AiOutlineNodeIndex } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center p-5 bg-white border-2 fixed top-0 left-0 right-0">
+        
+        <Link 
+            to="/" 
+            className='flex justify-start items-center text-xl text-black space-x-1'>
+            <span>WellFund</span>
+            <span className='text-emerald-600'><AiOutlineNodeIndex/></span>
+        </Link>
+
+        <div className='flex space-x-2 justify-center'>
+            <button type='button' className='inline-block bg-green-600 px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'>Connect Wallet</button>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+const Hero = () => {
+  return (
+    <div className="py-24 px-6 text-center bg-white">
+        <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
+            <span className="capitalize">Bring Creative Projects to life on</span>
+            <br />
+            <span className="text-green-600">WellFund</span>
+        </h1>
+        
+        <div className="flex justify-center items-center space-x-2">
+            <button type='button' className='inline-block bg-green-600 px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700'>Add New Project</button>
+            
+            <button type='button' className='inline-block border border-green-500 px-6 py-2.5 font-medium text-xs leading-tight uppercase rounded-full shadow-md bg-transparent text-green-600 hover:text-white hover:bg-green-600'>Fund Existing Project</button>
+        </div>
+
+        <div className="flex justify-evenly space-x-2 items-center mt-10">
+            <div className="flex flex-col justify-center items-center h-20 border border-gray-200 shadow-sm w-full rounded-md">
+                <span className="text-lg font-bold text-green-900 leading-5">{0}</span>
+                <span>Projects</span>
+            </div>
+            <div className="flex flex-col justify-center items-center h-20 border border-gray-200 shadow-sm w-full rounded-md">
+                <span className="text-lg font-bold text-green-900 leading-5">{0}</span>
+                <span>Backings</span>
+            </div>
+            <div className="flex flex-col justify-center items-center h-20 border border-gray-200 shadow-sm w-full rounded-md">
+                <span className="text-lg font-bold text-green-900 leading-5">{0} ETH</span>
+                <span>Donated</span>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
